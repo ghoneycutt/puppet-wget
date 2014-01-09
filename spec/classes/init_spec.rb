6e8d3213b7aa8d3fd6247873e167d706a345f6ab
Garrett Honeycutt <code@garretthoneycutt.com>
2014-01-08 19:26:14 -0500

    Fix spec tests
    Previously spec tests were failing all over the show.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -10,48 +10,100 @@ describe 'wget' do
   context 'running on CentOS' do
     let(:facts) { {:osfamily => 'RedHat'} }
 
-    it { should contain_package('wget') }
+    it {
+      should contain_package('wget').with({
+        'ensure' => 'installed',
+        'name'   => 'wget',
+      })
+    }
   end
 
   context 'running on Suse' do
     let(:facts) { {:osfamily => 'Suse'} }
 
-    it { should contain_package('wget') }
+    it {
+      should contain_package('wget').with({
+        'ensure' => 'installed',
+        'name'   => 'wget',
+      })
+    }
   end
 
-  context 'running on Solaris' do
-    let (:facts) { {:osfamily => 'Solaris', :kenerlrelease => '5.9'} }
+  context 'running on Solaris 9' do
+    let(:facts) do
+      { :osfamily => 'Solaris',
+        :kenerlrelease => '5.9',
+      }
+    end
 
-    it { should contain_package([ 'SUNWwgetr', 'SUNWwgetu' ]) }
+    it {
+      should contain_package('wget').with({
+        'ensure' => 'installed',
+        'name'   => ['SUNWwgetr', 'SUNWwgetu'],
+      })
+    }
   end
 
-  context 'running on Solaris' do
-    let (:facts) { {:osfamily => 'Solaris', :kenerlrelease => '5.11'} }
+  context 'running on Solaris 10' do
+    let(:facts) do
+      { :osfamily => 'Solaris',
+        :kenerlrelease => '5.10',
+      }
+    end
 
-    it { should contain_package('SUNWwget') }
+    it {
+      should contain_package('wget').with({
+        'ensure' => 'installed',
+        'name'   => ['SUNWwgetr', 'SUNWwgetu'],
+      })
+    }
+  end
+
+  context 'running on Solaris 11' do
+    let(:facts) do
+      { :osfamily => 'Solaris',
+        :kernelrelease => '5.11',
+      }
+    end
+
+    it {
+      should contain_package('wget').with({
+        'ensure' => 'installed',
+        'name'   => 'SUNWwget',
+      })
+    }
   end
 
 
   context 'no version specified' do
+    let(:facts) { { :osfamily => 'RedHat' } }
+
     it { should contain_package('wget').with_ensure('installed') }
   end
 
   context 'version is 1.2.3' do
     let(:params) { {:version => '1.2.3'} }
+    let(:facts) { { :osfamily => 'RedHat' } }
 
     it { should contain_package('wget').with_ensure('1.2.3') }
   end
 
   describe 'wget::fetch' do
-    it { should contain_exec('wget-test').with_command('wget --no-verbose --output-document=/tmp/dest http://localhost/source') }
+    let(:facts) { { :osfamily => 'RedHat' } }
+
+    it { should contain_exec('wget-test') \
+      .with_command('wget --no-verbose --output-document=/tmp/dest http://localhost/source') }
   end
 
   describe 'wget::authfetch' do
+    let(:facts) { { :osfamily => 'RedHat' } }
+
     it { should contain_exec('wget-authtest').with({
       'command'     => 'wget --no-verbose --user=myuser --output-document=/tmp/dest http://localhost/source',
       'environment' => 'WGETRC=/tmp/wgetrc-authtest'
       })
     }
+
     it { should contain_file('/tmp/wgetrc-authtest').with_content('password=mypassword') }
   end
 end
